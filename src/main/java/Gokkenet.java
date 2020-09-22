53cb3c46e6835d328db2341913b88f3259be82ff
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2020-09-22 14:30:46 +0200

    sql virker nu

--- src/main/java/Gokkenet.java
+++ src/main/java/Gokkenet.java
@@ -1,6 +1,9 @@
 import processing.core.PApplet;
+import de.bezier.data.sql.*;
 
 public class Gokkenet extends PApplet {
+
+    SQLite db;
     public static void main(String[] args) {
         PApplet.main("Gokkenet");
     }
