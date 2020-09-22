67a64499534c055b3c2fbfdd3dee647f9ac1bd71
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2020-09-22 15:14:18 +0200

    ok vi vælger at bruge JDBC istedet

--- src/main/java/Gokkenet.java
+++ src/main/java/Gokkenet.java
@@ -1,10 +1,16 @@
 import processing.core.PApplet;
-import de.bezier.data.sql.*;
+
 
 public class Gokkenet extends PApplet {
 
-    SQLite db;
+
     public static void main(String[] args) {
         PApplet.main("Gokkenet");
     }
+
+    @Override
+    public void setup() {
+
+    }
+
 }
