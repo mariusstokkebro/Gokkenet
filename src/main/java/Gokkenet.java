33e93e59d0adf922b2b55a42855e90c105c3d4fb
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2020-09-25 12:07:29 +0200

    tt

--- src/main/java/Gokkenet.java
+++ src/main/java/Gokkenet.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import java.sql.*;
 
 
 public class Gokkenet extends PApplet {
@@ -7,6 +8,7 @@ public class Gokkenet extends PApplet {
     public static void main(String[] args) {
         PApplet.main("Gokkenet");
     }
+    String databaseURL = "jdbc:ucanaccess://e://Java//JavaSE//MsAccess//Contacts.accdb";
 
     @Override
     public void setup() {
