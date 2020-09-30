43293a93c6b3f3228984988e1e55e73a83a2dd48
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2020-09-30 13:09:53 +0200

    lortet virker ikke

--- src/main/java/Gokkenet.java
+++ src/main/java/Gokkenet.java
@@ -8,16 +8,25 @@ public class Gokkenet extends PApplet {
 
     public static void main(String[] args) {
         PApplet.main("Gokkenet");
-
-
     }
 
+    LoginSide ls;
 
-
-
+    @Override
+    public void settings() {
+        size(500,500);
+    }
 
     @Override
     public void setup() {
+
+        ls = new LoginSide(this);
+    }
+
+    @Override
+    public void draw() {
+        ls.drawSide();
+
         String databaseURL = "jdbc:ucanaccess://src//main//java//resources//database.accdb";
         try {
             Connection connection = DriverManager.getConnection(databaseURL);
@@ -32,17 +41,26 @@ public class Gokkenet extends PApplet {
 
                 System.out.println(rp.getString(1));
                 System.out.println("");
+                if(ls.userName.indput.equals(rs.getString(1) && ls.password.indput.equals(rp.getString(1))){
+                    println("fuck");
+                    ls.visible = false;
+                }
 
             }
+
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
-    public void draw() {
+    public void keyTyped() {
+        ls.typede(key);
+    }
 
+    @Override
+    public void mouseClicked() {
+        ls.clik(mouseX,mouseY);
     }
 }
--- src/main/java/LoginSide.java
+++ src/main/java/LoginSide.java
@@ -1,16 +1,34 @@
 import processing.core.PApplet;
 
 public class LoginSide {
- PApplet p;
- TextFlet password;
- TextFlet userName;
-    LoginSide(PApplet p){
-      this.p = p;
-      password = new TextFlet(p,p.width/2,p.height/2, 200, 50, "Fuck");
+    PApplet p;
+    TextFlet password;
+    TextFlet userName;
+    boolean visible = true;
 
+    LoginSide(PApplet p) {
+        this.p = p;
+        password = new TextFlet(p, p.width / 2, p.height / 2, 200, 50, "Password");
+        userName = new TextFlet(p, p.width / 2, p.height / 2 - 60, 200, 50, "userName");
     }
 
-    void drawSide(){
+    void drawSide() {
+        if (visible) {
+            password.tegnTextFlet();
+            userName.tegnTextFlet();
+        }
+    }
 
+
+    void typede(char key) {
+        if (visible) {
+            password.keyindput(key);
+            userName.keyindput(key);
+        }
+    }
+
+    void clik(float mx, float my){
+        password.KlikTjek(mx,my);
+        userName.KlikTjek(mx,my);
     }
 }
