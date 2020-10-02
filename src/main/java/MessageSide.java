6574e6c781ee1bf97f219b0bf26c69eb20fb3dca
Christian Randleff <cra@minuba.dk>
2020-10-02 14:35:15 +0200

    tilføjet flere classer

--- src/main/java/MessageSide.java
+++ src/main/java/MessageSide.java
@@ -2,12 +2,14 @@ import processing.core.PApplet;
 
 public class MessageSide {
     PApplet p;
+    TextFlet besked;
 
     MessageSide(PApplet p){
         this.p = p;
+        besked = new TextFlet(p, p.width / 2, p.height / 2 - 70, 200, 50, "Beskeder");
     }
 
     public void drawMessahe(){
-
+        besked.tegnTextFlet();
     }
 }
